2e8beef8022ca56ea730e38001b247ed622d68b7
Jen <earth.terminal@gmail.com>
2013-04-20 18:04:05 -0700

    trying to make the save button work on the photo tab in admin page so that it will show the name, date and text fields next to the photo tumbnail when it is saved. the js kind of works but does not stay on the photo tab after hitting save and does not display the inputs...the hide/show work but yeah...out of practice. also added an 'add file' button which does not work. can probably leave it just for show. i think i only made some tweaks to the admin-relevant css.

---
+++ js/admin.js
@@ -0,0 +1,23 @@
+//admin.html js 
+
+//this function is working but i have to figure out how to get it to stay on the tab
+
+$(document).ready(function(){
+	$("#saved_content").hide();
+	$("form").submit(function(){
+	
+			photo_inputs();
+			$("#saved_content").show();
+			$("form").hide();
+
+	});
+});
+
+function photo_inputs() {
+	//name input
+	$(".input_name").empty().text($("#name").val() );
+	//date input
+	$(".input_date").empty().text($("#date").val() );
+	//description input
+	$(".input_desc").empty().text($("#desc").val() );
+	}
