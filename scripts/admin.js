c15093683825ddd3450012bfb763149733b7cf09
Elliot Nahman <elliot@Cora.local>
2013-04-27 12:18:53 -0700

    all edit buttons now work and bring up a edit form

--- scripts/admin.js
+++ scripts/admin.js
@@ -6,19 +6,46 @@ $(document).ready(function(){
     $(this).css('background-image','url(images/story/'+f+')');
   });
 
+  $('.thumbnail').imageZoom();
+
+  $('.saved_audio').on('click','.edit_audio',function(){
+    var audioData = $(this).parents('.saved_audio');
+    var pdisplay = audioData.children('.display_slide');
+    var pform = audioData.children('.form_slide');
+
+    var title = pdisplay.find('h2.no_margin').text();
+    pform.find('.audio_title').val(title);
+
+    audioData.css('height','110px');
+    slideOutIn(pdisplay,pform);
+  });
+
+  $('.saved_audio').on('click','.submitting',function(e){
+    var audioData = $(this).parents('.saved_audio');
+    var pdisplay = audioData.children('.display_slide');
+    var pform = audioData.children('.form_slide');
+
+    var title = pform.find('.audio_title').val();
+    pdisplay.find('h2.no_margin').text(title);
+
+    audioData.css('height','auto');
+    slideOutIn(pdisplay,pform);
+    e.preventDefault();
+  });
+
   $('.saved_photo').on('click','.edit_photo',function(){
     console.log('edit');
     var photoData = $(this).parents('.photo_data');
     var pdisplay = photoData.children('.display_slide');
     var pform = photoData.children('.form_slide');
 
-    var name = pdisplay.children('.photo_name').text();
-    var date = pdisplay.children('.photo_date').text();
-    var desc = pdisplay.children('.photo_desc').text();
+    var name = pdisplay.find('.photo_name').text();
+    var date = convertDateToForm(pdisplay.find('.photo_date').text());
+    var desc = pdisplay.find('.photo_desc').text();
 
-    pform.children('.photo_name').val(name);
-    pform.children('.photo_date').val(date);
-    pform.children('.photo_desc').val(desc);
+    pform.find('.photo_name').val(name);
+    pform.find('.photo_date').val(date);
+    pform.find('.photo_desc').val(desc);
 
     slideOutIn(pdisplay,pform);
   });
@@ -28,29 +55,16 @@ $(document).ready(function(){
     var pdisplay = photoData.children('.display_slide');
     var pform = photoData.children('.form_slide');
     
-    var name = pform.children('.photo_name').val();
-    var date = pform.children('.photo_date').val();
-    var desc = pform.children('.photo_desc').val();    
+    var name = pform.find('.photo_name').val();
+    var date = convertDateFromForm(pform.find('.photo_date').val());
+    var desc = pform.find('.photo_desc').val();    
 
-    pdisplay.children('.photo_name').text(name);
-    pdisplay.children('.photo_date').text(date);
-    pdisplay.children('.photo_desc').text(desc);
+    pdisplay.find('.photo_name').text(name);
+    pdisplay.find('.photo_date').text(date);
+    pdisplay.find('.photo_desc').text(desc);
 
     slideOutIn(pdisplay, pform);
     e.preventDefault();
-  });
-
-  $("#video").submit(function(){
-  
-      photo_inputs();
-      $("#video").hide();
-      $("#saved_video").show();
-
-      alert ("it worked?")
-      return false;
-      //blank forms need to remain unchanged
-      //need to show what IS submitted
-
   });
 });
 
@@ -59,11 +73,46 @@ function slideOutIn(inElement,outElement) {
   inElement.toggle('slide', { direction:'left'},500);
 }
 
-function photo_inputs() {
-  //name input
-  $(".input_name").empty().text($("#name").val() );
-  //date input
-  $(".input_date").empty().text($("#date").val() );
-  //description input
-  $(".input_desc").empty().text($("#desc").val() );
+function convertDateToForm(str){
+  // str = 5 March 2013
+  // --> 2013-03-05
+  var months = {
+    "January":'01', 
+    "February":'02', 
+    "March":'03', 
+    "April":'04', 
+    "May":'05', 
+    "June":'06', 
+    "July":'07', 
+    "August":'08', 
+    "September":'09', 
+    "October":'10', 
+    "November":'11', 
+    "December":'12'
+  };
+  var strArr = str.split(' ');
+  var day = (strArr[0].length == 2) ? strArr[0] : '0'+strArr[0];
+  return ''+strArr[2]+'-'+months[strArr[1]]+'-'+day
+}
+
+function convertDateFromForm(str){
+  // str = 2013-03-05
+  // --> 5 March 2013
+  var months = {
+    '01':"January", 
+    '02':"February", 
+    '03':"March", 
+    '04':"April", 
+    '05':"May", 
+    '06':"June",
+    '07':"July", 
+    '08':"August", 
+    '09':"September", 
+    '10':"October", 
+    '11':"November", 
+    '12':"December"
+  };
+  var strArr = str.split('-');
+  var day = (strArr[2][0] == '0') ? strArr[2][1] : strArr[2];
+  return ''+day+' '+months[strArr[1]]+' '+strArr[0]
 }
