eca63d1620891e2872355ad2f7508e207703b793
J. Elliot Nahman <elliot@ischool.berkeley.edu>
2013-05-17 10:13:02 -0700

    changes to main pages

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,5 @@ $(document).ready(function(){
   var h1 = $('.white_rect').outerHeight(),
       h2 = $('.index_pic').innerHeight();
   var hx = (h2-h1)/2;
-  console.log(h1);
-  console.log(h2);
-  console.log(hx);
   $('.white_rect').css('margin-top',hx);
 });
