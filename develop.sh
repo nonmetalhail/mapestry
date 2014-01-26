fc578e78fe5039f29e76fe2f40481fefeea2b00b
Scott Garrison <scott@allpowerlabs.org>
2014-01-25 16:55:39 -0800

    specified components

--- develop.sh
+++ develop.sh
@@ -7,7 +7,14 @@ set -e
 
 DIR=$(cd $(dirname $0); pwd)
 
+if which virtualenv-2.7 &>/dev/null
+then
+   VIRTUALENV=virtualenv-2.7
+else
+   VIRTUALENV=virtualenv
+fi
+
 mkdir -p $DIR/build
-virtualenv $DIR/build/virtualenv
+$VIRTUALENV $DIR/build/virtualenv
 $DIR/build/virtualenv/bin/pip install -r $DIR/requirements.txt
 
