fc98027293cd43fcfea54e2950159bc69bc65c26
Scott Garrison <scott@allpowerlabs.org>
2014-03-02 21:59:05 -0800

    Story add entry

--- mapestry/flaskapp.py
+++ mapestry/flaskapp.py
@@ -1,7 +1,7 @@
 from flask import Flask, make_response, flash, request, render_template, redirect, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy.orm.exc import NoResultFound #, MultipleResultsFound
-import hashlib, json
+import json, datetime
 
 from flask.ext.login import LoginManager, login_user, logout_user, login_required
 import hashlib
@@ -65,8 +65,8 @@ def check_hash(password, hash_):
     for char_a, char_b in izip(hash_a, hash_b):
         diff |= ord(char_a) ^ ord(char_b)
     return diff == 0
-    
-    
+
+
 #
 # Models
 #
@@ -76,7 +76,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120))
     password = db.Column(db.String(64))
-    
+
     def __init__(self, email, password):
         self.email = email
         self.password = make_hash(password)
@@ -95,7 +95,7 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.email
-        
+
     def __unicode__(self):
         return unicode(self.email)
 
@@ -103,7 +103,7 @@ story_tags_table = db.Table('story_tags', db.Model.metadata,
                            db.Column('story_id', db.Integer, db.ForeignKey('stories.id')),
                            db.Column('tag_id', db.Integer, db.ForeignKey('tags.id'))
                            )
-                           
+
 class Story(db.Model):
     __tablename__ = 'stories'
     id = db.Column(db.Integer, primary_key=True)
@@ -113,10 +113,10 @@ class Story(db.Model):
     date = db.Column(db.Date)
     text = db.Column(db.Text, unique=True)
     segments = db.relationship('Segment', back_populates="story")
-    
+
     def __init__(self, story, audio, title, date, text, tags):
-        self.story = story      
-        self.audio = audio 
+        self.story = story
+        self.audio = audio
         self.title = title
         self.date = date
         self.text = text
@@ -125,25 +125,26 @@ class Story(db.Model):
         self.tags = [
             Tag(tag_text) for tag_text in tags
             ]
-                                    
+
     def __repr__(self):
         return '<Story %r>' % self.title
-        
-                                   
+
+
 class Tag(db.Model):
     __tablename__ = 'tags'
     id = db.Column(db.Integer, primary_key=True)
     tag_text = db.Column(db.String(80))
     story_id = db.Column(db.Integer, db.ForeignKey('stories.id'))
-    story = db.relationship('Story', secondary='story_tags', backref=db.backref('tags'))  
-    
+    story = db.relationship('Story', secondary='story_tags', backref=db.backref('tags'))
+
     def __init__(self, tag_text):
         self.tag_text = tag_text
         #self.story = story
-        
+
     def __repr(self):
         return '<Tag %r>' % self.tag_text
-        
+
+
 class Photo(db.Model):
     __tablename__ = 'photos'
     id = db.Column(db.String(32), primary_key=True) #md5
@@ -152,9 +153,9 @@ class Photo(db.Model):
     date = db.Column(db.Date)  #db.DateTime, default=datetime.datetime.now)
     description = db.Column(db.String(256))
     stories = db.Column(db.String(256))
-    share = db.Column(db.String(80))  #Boolean?  then use: "Public" if p.share else "Private" 
-    
-    
+    share = db.Column(db.String(80))  #Boolean?  then use: "Public" if p.share else "Private"
+
+
     def __init__(self, image, name, date, description, stories, share):
         md5 = hashlib.md5()
         md5.update(image)
@@ -164,11 +165,12 @@ class Photo(db.Model):
         self.date = date
         self.description = description
         self.stories = stories
-        self.share = share  
-        
+        self.share = share
+
     def __repr__(self):
        return '<Photo %r>' % self.name
-    
+
+
 class Audio(db.Model):
     id = db.Column(db.String(32), primary_key=True)
     audio = db.Column(db.String(80), unique=True)
@@ -180,10 +182,11 @@ class Audio(db.Model):
         self.id = md5.hexdigest()
         self.audio = audio
         self.name = name
-    
+
     def __repr__(self):
        return '<Audio %r>' % self.name
-    
+
+
 class Location(db.Model):
     __tablename__ = 'locations'
     id = db.Column(db.Integer, primary_key=True)
@@ -197,33 +200,34 @@ class Location(db.Model):
     country = db.Column(db.String(128))
     share = db.Column(db.String(80))
     stories = db.Column(db.String(128))
-    
+
     def __init__(self, lat, lng, title, address, city, state, zip, country, share, stories):
         self.lat = lat
-        self.lng = lng 
-        self.title = title 
-        self.address = address 
-        self.city = city 
-        self.state = state 
-        self.zip = zip 
-        self.country = country 
-        self.share = share  
+        self.lng = lng
+        self.title = title
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip = zip
+        self.country = country
+        self.share = share
         self.stories = stories
- 
+
     def __repr__(self):
        return '<Location %r>' % self.title
+
 
 class Segment(db.Model):
     __tablename__ = 'segments'
     id = db.Column(db.Integer, primary_key=True)
     story_id = db.Column(db.Integer, db.ForeignKey('stories.id'))
     story = db.relationship('Story', back_populates="segments") # backref=db.backref('segments')
-    title = db.Column(db.String(128), unique=True)   
+    title = db.Column(db.String(128), unique=True)
     time = db.Column(db.Integer)
     desc = db.Column(db.String(256))
     lat = db.Column(db.Float)
     long = db.Column(db.Float)
-    
+
     def __init__(self, story_id, title, time, desc, latlong):
         self.story_id = story_id
         self.title = title
@@ -231,7 +235,7 @@ class Segment(db.Model):
         self.desc = desc
         self.lat = latlong[0]
         self.long = latlong[1]
-    
+
     def __repr__(self):
         return '<Segment %r>' % self.title
 
@@ -239,32 +243,37 @@ class Segment(db.Model):
 #
 # Helpers
 #
-   
+
 def json_response(response_dict):
     '''Takes dict or list of dicts and returns a jsonified response'''
     response_json = json.dumps(response_dict)
     response = make_response(response_json)
     response.headers['content_type'] = 'application/json'
-    return response    
+    return response
 
+def date_from_form(field):
+    date_components = [int(x) for x in field.split('-')] 
+    return datetime.date(*date_components)
+    
 def try_login_user(email, password):
     if not password:
         return None
-    try: 
-        user = User.query.filter_by(email=email).one() 
+    try:
+        user = User.query.filter_by(email=email).one()
     except NoResultFound:
         return None
     if not check_hash(password, user.password):
         return None
     login_user(user)
     return user
-    
+
 
 @login_manager.user_loader
 def load_user(userid):
     return User.query.get(int(userid))
 
-login_manager.login_view = "login"   
+login_manager.login_view = "login"
+
 
 #
 # Views
@@ -274,7 +283,7 @@ login_manager.login_view = "login"
 @app.route('/index')
 def index():
     """Return the main view."""
-    return render_template('index.html')    
+    return render_template('index.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -291,16 +300,18 @@ def login():
             return redirect(url_for('index'))
     return render_template('login.html', error=error)
 
-    
+
 @app.route('/logout')
 def logout():
     logout_user()
     flash(u'You were logged out')
     return redirect(url_for('index'))
 
+
 #
 # File Upload
 #
+
 app.config["UPLOADED_PHOTOS_DEST"] = "photos"
 app.config["UPLOADED_AUDIO_DEST"] = "audio"
 
@@ -312,25 +323,25 @@ configure_uploads(app, (photos, audio_files))
 @login_required
 def upload():
     stories_dict = [{"id":repr(s.id), "story": s.story} for s in Story.query.all()]
-    return render_template('upload.html', stories = stories_dict)
+    audio_dict =[{"id":repr(s.id), "name": a.name} for a in Audio.query.all()]
+    return render_template('upload.html', stories = stories_dict, audio_files=audio_dict)
 
 @app.route('/upload/photo/', methods=['GET', 'POST'])
 @login_required
 def photo_upload():
     if request.method == 'POST' and 'photo' in request.files:
         filename = photos.save(request.files['photo'])
-        photo = Photo(image=filename, 
-                    name = request.form['name'], 
-                    date = request.form['date'], 
-                    description = request.form['description'], 
-                    stories = request.form['stories'], 
+        photo = Photo(image=filename,
+                    name = request.form['name'],
+                    date = date_from_form(request.form['date']),
+                    description = request.form['description'],
+                    stories = request.form['stories'],
                     share = request.form['share'])
         db.session.add(photo)
         db.session.commit()
         flash(u"Photo saved.")
         #return redirect(url_for('show', id=photo.id))
-    return render_template('upload.html')
-    
+    return redirect(url_for('upload'))
 
 @app.route('/upload/audio/', methods=['GET', 'POST'])
 @login_required
@@ -342,58 +353,78 @@ def audio_upload():
         db.session.commit()
         flash(u"Audio file saved.")
         #return redirect(url_for('listen', id=rec.id))
-    return render_template('upload.html')
+    return redirect(url_for('upload'))
     
-@app.route('/photo/<id>')
+@app.route('/upload/story/', methods=['GET', 'POST'])
+@login_required
+def add_entry():
+    if request.method == 'POST':
+        entry = Story(story_text = request.form['story'],
+                  audio_number=request.form['audio'],
+                  title = request.form['title'],
+                  date = date_from_form(request.form['date']),
+                  text = request.form['text'],
+                  tags = request.form['tags'].split(",")
+                 )
+    db.session.add(entry)
+    db.commit()
+    flash(u'New entry was successfully posted')
+    return redirect(url_for('upload'))  #set to 'view_stories' later
+    
+
+@app.route('/photo/<id>') #Not tested
 def show(id):
     photo = Photo.load(id)
     if photo is None:
         abort(404)
     url = photos.url(photo.filename)
     return render_template('show.html', url=url, photo=photo)
-    
+
 
 #
 # API methods
 #
-        
-@app.route('/api/story/', methods=['GET'])       
+
+@app.route('/api/story/', methods=['GET'])
 def get_stories():
-    stories = [{repr(s.id):{"story": s.story, 
-                            "audio": s.audio, 
-                            "storyTitle": s.title, 
-                            "rDate": str(s.date), 
-                            "sText": s.text, 
+    try:
+        stories = [{repr(s.id):{"story": s.story,
+                            "audio": s.audio,
+                            "storyTitle": s.title,
+                            "rDate": str(s.date),
+                            "sText": s.text,
                             "sTags": [tag.tag_text for tag in s.tags]
                             }
-                } for s in Story.query.all()] 
+                    } for s in Story.query.all()]
+    except NoResultFound:
+        return None
     return json_response(stories)
 
 @app.route('/api/story/<story>', methods=['GET'])
 def show_story(story):
-    story = Story.query.filter_by(story=story).first()
-    story_dict = {repr(story.id):{"story": story.story, 
-                                  "audio": story.audio, 
-                                  "storyTitle": story.title, 
-                                  "rDate": str(story.date), 
-                                  "sText": story.text, 
-                                  "sTags": [tag.tag_text for tag in s.tags] 
+    story = Story.query.filter_by(story=story).all() #.first()
+    story_dict = {repr(story.id):{"story": story.story,
+                                  "audio": story.audio,
+                                  "storyTitle": story.title,
+                                  "rDate": str(story.date),
+                                  "sText": story.text,
+                                  "sTags": [tag.tag_text for tag in story.tags]
                                   }
                   }
-    story_dict[repr(story.id)]["segment"] = [{"segTitle":seg.title, 
-                                              "time":seg.time, 
-                                              "desc":seg.desc, 
+    story_dict[repr(story.id)]["segment"] = [{"segTitle":seg.title,
+                                              "time":seg.time,
+                                              "desc":seg.desc,
                                               "latlong":[seg.lat, seg.long]
                                               } for seg in Segment.query.filter_by(story_id=repr(story.id)).all()]
     return json_response(story_dict)
-        
+
 @app.route('/api/photo/', methods=['GET'])
 def get_photos():
-    photos = [{repr(p.id):{"i":p.image, 
-                           "n":p.name, 
-                           "date":str(p.date), 
-                           "d":p.description, 
-                           "s":p.stories, 
+    photos = [{repr(p.id):{"i":p.image,
+                           "n":p.name,
+                           "date":str(p.date),
+                           "d":p.description,
+                           "s":p.stories,
                            "share": p.share
                            }
                } for p in Photo.query.all()]
@@ -402,29 +433,13 @@ def get_photos():
 @app.route('/api/photo/<photo>', methods=['GET'])
 def show_photo(photo):
     photo = Photo.query.filter_by(photo=photo).first()
-    photo_dict = {repr(photo.id):{"i":photo.image, 
+    photo_dict = {repr(photo.id):{"i":photo.image,
                                    "n":photo.name,
                                    "date":str(photo.date) ,
                                    "d":photo.description ,
                                    "s":photo.stories ,
                                    "share":photo.share}}
-    return json_response(photo_dict)    
-
-@app.route('/api/story/add', methods=['POST'])
-@login_required
-def add_entry():
-    if not session.get('logged_in'):
-        abort(401)
-    db = get_db()
-    db.execute('insert into stories (story, audio, title, date, text) values (?, ?)',
-                [request.form['story'],
-                 request.form['audio'], 
-                 request.form['title'], 
-                 request.form['date'], 
-                 request.form['text']])
-    db.commit()
-    flash(u'New entry was successfully posted')
-    return redirect(url_for('show_entries'))
+    return json_response(photo_dict)
 
 
 @app.errorhandler(404)
